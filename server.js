bdd441d2c8769c8c47266ddc7339628a36c1bf13
Manish-coderdotcom <manishdubey70380@gmail.com>
2026-08-25 17:08:15 +0530

    Uncomment server listen and log message

--- server.js
+++ server.js
@@ -98,6 +98,6 @@ app.get("/", (req, res) => {
 });
 
 // Start the server
-//app.listen(PORT, () => {
-   // console.log(`Server is running on http://localhost:${PORT}`);
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
 });
